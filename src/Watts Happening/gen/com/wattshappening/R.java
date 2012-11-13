88ef48c2e73e379e2ba3a16946c1df1d17f1d1a6
bbramble <bbramble@cs.wisc.edu>
2012-11-12 21:00:33 -0600

    I created a GPSservice that tries to sync up with the DB.  It has an error adding the coordinate though.

--- src/Watts Happening/gen/com/wattshappening/R.java
+++ src/Watts Happening/gen/com/wattshappening/R.java
@@ -17,7 +17,9 @@ public final class R {
     public static final class id {
         public static final int button1=0x7f070000;
         public static final int button2=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int button3=0x7f070002;
+        public static final int button4=0x7f070003;
+        public static final int menu_settings=0x7f070004;
     }
     public static final class layout {
         public static final int activity_watts_happening=0x7f030000;
@@ -29,7 +31,9 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int start_GPS_button=0x7f040006;
         public static final int start_button=0x7f040004;
+        public static final int stop_GPS_button=0x7f040007;
         public static final int stop_button=0x7f040005;
         public static final int title_activity_watts_happening=0x7f040003;
     }
