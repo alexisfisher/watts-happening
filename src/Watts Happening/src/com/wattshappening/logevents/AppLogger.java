179a1d27053ac7721cd4b94001460dfac516ed11
Adam Vail <adam.r.vail@gmail.com>
2012-11-15 13:50:22 -0600

    Added function for parsing the dumpsys cpuinfo command and putting it in the database

--- src/Watts Happening/src/com/wattshappening/logevents/AppLogger.java
+++ src/Watts Happening/src/com/wattshappening/logevents/AppLogger.java
@@ -1,6 +1,11 @@
 package com.wattshappening.logevents;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Vector;
 
 import android.app.ActivityManager;
 import android.app.Service;
@@ -32,6 +37,38 @@ public class AppLogger extends LogProcess {
 		// TODO Auto-generated method stub
 
 	}
+	
+	private HashMap<String, Double> parseDumpSys(){
+		BufferedReader in = null;
+		HashMap<String, Double> cpuInfo = new HashMap<String, Double>();
+		Process process = null;
+		try{
+			process = Runtime.getRuntime().exec("dumpsys cpuinfo");
+			in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line = "";
+			while((line = in.readLine()) != null){
+				String [] contents = line.split(" ");
+				String processName = contents[1].substring(contents[1].indexOf('/') + 1);
+				for(String s : contents){
+					Log.i("AppLogging", "Contents: " + s);
+				}
+				Double usage = Double.parseDouble(contents[0].substring(0, contents[0].indexOf('%')));
+				cpuInfo.put(processName, usage);
+			}
+			
+		}catch(IOException e){
+			Log.e("AppLoggingDumpSys", e.getMessage());
+		}finally {
+			try{
+				in.close();
+				process.destroy();
+			} catch(IOException e){
+				Log.e("AppLoggingDumpSys", e.getMessage());
+			}
+		}
+		
+		return cpuInfo;
+	}
 
 	@Override
 	protected void logInformation() {
@@ -39,25 +76,34 @@ public class AppLogger extends LogProcess {
 		PackageManager pm = parent.getPackageManager();
 		List<ActivityManager.RunningAppProcessInfo> procs = am.getRunningAppProcesses();
 		if(procs != null){
+			HashMap<String, Double> usage = parseDumpSys();
 			for(ActivityManager.RunningAppProcessInfo proc : procs){
 				ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo)(proc);
 				String name = info.processName;
-				Log.i("AppLogging", name);
+				
 				try {
 					CharSequence c = pm.getApplicationLabel(pm.getApplicationInfo(
 							info.processName, PackageManager.GET_META_DATA));
 					name = c.toString();
 					Log.i("AppLogging", c.toString());
 				} catch (NameNotFoundException e) {
-					Log.e("AppLogging: ", e.getMessage());
+					// used for testing initially, doesn't really matter to see this now
+					//Log.e("AppLogging: ", e.getMessage());
 				}				
 				int pid = info.pid;
+				
+				Double cpu = usage.get(info.processName);
+				if(cpu == null){
+					Log.e("AppLogging", info.processName + " not found in dumpsys!");
+					cpu = 0.0;
+				}
+				
 				try {
 					// for now don't put cpu info in, decide if we want to parse
 					// top to figure this out, might be too heavy for what we're doing
-					ait.addEntry(new AppInfo(name, pid, 0));
+					ait.addEntry(new AppInfo(name, pid, cpu));
 				} catch (Exception e) {
-					Log.e("App Logging: ", e.getMessage());
+					Log.e("AppLogging: ", e.getMessage());
 				}
 			}
 		}
