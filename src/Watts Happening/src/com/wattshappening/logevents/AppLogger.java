64324ffa5c78778d57170a5054bda22008f3fa74
Adam Vail <adam.r.vail@gmail.com>
2012-11-14 19:55:38 -0600

    Added app logger, need to talk about if we want to parse top for cpu usage

---
+++ src/Watts Happening/src/com/wattshappening/logevents/AppLogger.java
@@ -0,0 +1,66 @@
+package com.wattshappening.logevents;
+
+import java.util.Iterator;
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
+
+import com.wattshappening.db.AppInfo;
+import com.wattshappening.db.AppInfoTable;
+
+public class AppLogger extends LogProcess {
+
+	AppInfoTable ait = null;
+	
+	public AppLogger(Service parent){
+		super(parent, 10000);
+		ait = new AppInfoTable(parent);
+	}
+	
+	@Override
+	protected void startLoggingEvents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void stopLoggingEvents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void logInformation() {
+		ActivityManager am = (ActivityManager)parent.getSystemService(Context.ACTIVITY_SERVICE);
+		PackageManager pm = parent.getPackageManager();
+		List<ActivityManager.RunningAppProcessInfo> l = am.getRunningAppProcesses();
+		if(l != null){
+			Iterator<ActivityManager.RunningAppProcessInfo> i = l.iterator();
+			while(i.hasNext()){
+				ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo)(i.next());
+				String name = null;
+				try {
+					CharSequence c = pm.getApplicationLabel(pm.getApplicationInfo(
+							info.processName, PackageManager.GET_META_DATA));
+					name = c.toString();
+				} catch (NameNotFoundException e) {
+					Log.e("App Logging: ", e.getMessage());
+				}				
+				int pid = info.pid;
+				try {
+					// for now don't put cpu info in, decide if we want to parse
+					// top to figure this out, might be too heavy for what we're doing
+					ait.addEntry(new AppInfo(name, pid, 0));
+				} catch (Exception e) {
+					Log.e("App Logging: ", e.getMessage());
+				}
+			}
+		}
+	}
+
+}
