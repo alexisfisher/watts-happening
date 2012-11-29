abafc41355f2a423d4e3c0b1dd3d76346367c99a
alexisfisher <afisher@cs.wisc.edu>
2012-11-29 11:59:29 -0600

    App Analyzer now doesn't fail if no entries in db

--- src/Watts Happening/src/com/wattshappening/analysis/AppAnalyzer.java
+++ src/Watts Happening/src/com/wattshappening/analysis/AppAnalyzer.java
@@ -21,35 +21,37 @@ public class AppAnalyzer {
 	 * @return A double that's related to the usage of the app
 	 * 
 	 */
-	public static double analyzeApp(List<AppInfoBat> aib){
+	public static long analyzeApp(List<AppInfoBat> aib){
 		//we're given a list of appinfobat's about a single application
 		
-		double use = 0.0;
+		long use = 0;
 		int prevtime = 0;
 		ListIterator<AppInfoBat> aibIter = aib.listIterator();
-		AppInfoBat current = aibIter.next();
-		prevtime = current.getTimesliceID();
-		long prevCPUPercent = current.getCPU();
-		double prevBattLevel = current.getBatteryPercentage(); //the battery level during the last slice
-		double battDelta = 0.0; //The change in battery level between slices
-		double cpuDelta = 0.0; //How many CPU ticks were used during the last tick
-		
-		while (aibIter.hasNext()){
-			current = aibIter.next();
-			if (current.getTimesliceID() == prevtime + 1){
-			
-				battDelta = prevBattLevel - current.getBatteryPercentage();
-				cpuDelta = current.getCPU() - prevCPUPercent;
-				//Log.i("LocalService", "battdelta:"+battDelta + " cpuDelta: "+cpuDelta);
-				use += battDelta * cpuDelta; 	//does CPU need to be a delta, as well? 
-														//... yes  
-			}
+
+		if (true == aibIter.hasNext()){
+			AppInfoBat current = aibIter.next();
 			prevtime = current.getTimesliceID();
-			prevCPUPercent = current.getCPU();
-			prevBattLevel = current.getBatteryPercentage();
-			
+			long prevCPUPercent = current.getCPU();
+			double prevBattLevel = current.getBatteryPercentage(); //the battery level during the last slice
+			double battDelta = 0.0; //The change in battery level between slices
+			double cpuDelta = 0.0; //How many CPU ticks were used during the last tick
+
+			while (aibIter.hasNext()){
+				current = aibIter.next();
+				if (current.getTimesliceID() == prevtime + 1){
+
+					battDelta = prevBattLevel - current.getBatteryPercentage();
+					cpuDelta = current.getCPU() - prevCPUPercent;
+					//Log.i("LocalService", "battdelta:"+battDelta + " cpuDelta: "+cpuDelta);
+					use += battDelta * cpuDelta; 	//does CPU need to be a delta, as well? 
+					//... yes  
+				}
+				prevtime = current.getTimesliceID();
+				prevCPUPercent = current.getCPU();
+				prevBattLevel = current.getBatteryPercentage();
+
+			}
 		}
-		
 		//store in DB
 		return use;
 	}
